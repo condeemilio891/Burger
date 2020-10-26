f2ae78401331aead177399dd10eb652869a805aa
Emilio Conde <condeemilio891@gmail.com>
2020-10-25 21:29:42 -0500

    trying to push database again

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,24 @@
 var mysql = require("mysql");
 let mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL)
+  
+// } else {
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "f2fbe0zvg9j8p9ng.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 
   // Your port; if not 3306
   port: 3306,
 
   // Your username
-  user: "root",
+  user: "qm6y0pmz43xne7ma",
 
   // Your password
-  password: "yourpassword",
-  database: "BURGER_DB"
+  password: "koxdydwdo6e18cxt",
+  database: "e7mqg8wp7r0pqw84"
 });
-}
+
 connection.connect(function(err){
     if(err) throw err;})
 
